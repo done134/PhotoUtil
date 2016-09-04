392e219682f34cf3a09965d8b858bffc2ad5eb09
Done <done_yin@sina.com>
2016-09-04 21:20:46 +0800

    Initial commit

---
+++ app/src/main/java/com/done/photoutil/PhotoPickerActivity.java
@@ -0,0 +1,13 @@
+package com.done.photoutil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoPickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_picker);
+    }
+}
